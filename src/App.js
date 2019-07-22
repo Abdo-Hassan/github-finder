99c70a62525f8d7df7a3360be6b9131d18547382
abdo hassan <abdocoder0@gmail.com>
2019-07-22 15:06:33 +0200

    using spinner loading

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
   async componentDidMount() {
+    this.setState({ loading: true });
     const res = await axios.get('https://api.github.com/users');
-    console.log(res.data);
+    this.setState({ users: res.data, loading: false });
   }
 
   render() {
@@ -14,7 +19,7 @@ class App extends Component {
       <div>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
