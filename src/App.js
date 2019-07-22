0b20db368765545d0d395f69ea818a84f9574607
abdo hassan <abdocoder0@gmail.com>
2019-07-22 17:40:04 +0200

    make search users fearture

--- src/App.js
+++ src/App.js
@@ -3,22 +3,39 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import Search from './components/users/Search';
 class App extends Component {
   state = {
     users: [],
     loading: false
   };
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users');
-    this.setState({ users: res.data, loading: false });
-  }
+  // async componentDidMount() {
+  //   console.log(process.env.REACT_APP_GITGUB_CLIENT_SECRET);
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${
+  //       process.env.REACT_APP_GITGUB_CLIENT_ID
+  //     }&client_secret=${process.env.REACT_APP_GITGUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // search github users
+  searchUsers = async text => {
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITGUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITGUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div>
         <Navbar />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
---
+++ src/components/users/Search.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export class Search extends Component {
+  state = {
+    text: ''
+  };
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
+  render() {
+    return (
+      <div>
+        <form className='form' onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users'
+            value={this.state.text}
+            onChange={this.onChange}
+            autoFocus
+          />
+          <input
+            type='submit'
+            value='search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
