f4bbc61b38b0153199cb9df065a615f4ba5d4473
abdo hassan <abdocoder0@gmail.com>
2019-07-23 17:38:25 +0200

    made a repos page for users

---
+++ src/components/repos/Repos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RepoItem from './RepoItem';
+import PropTypes from 'prop-types';
+const Repos = ({ repos }) => {
+  return repos.map(repo => <RepoItem key={repo.id} repo={repo} />);
+};
+
+Repos.propTypes = {
+  repos: PropTypes.array.isRequired
+};
+
+export default Repos;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -2,16 +2,20 @@ import React, { Component, Fragment } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Repos from '../repos/Repos';
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
   };
 
   render() {
@@ -30,7 +34,7 @@ class User extends Component {
       public_gists,
       hireable
     } = this.props.user;
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
 
@@ -97,6 +101,7 @@ class User extends Component {
           <div className='badge badge-ligth'>Public Repos : {public_repos}</div>
           <div className='badge badge-dark'>Public Gists : {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
